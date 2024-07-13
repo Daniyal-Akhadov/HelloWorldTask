305a7cfb87a0ad40b37f1f3300e41d462cb145a4
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2024-07-13 19:35:46 +0400

    [homework]

---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+
+public class Cat {
+    private String name;
+
+    public Cat() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayMeow() {
+        System.out.println("Meow Meow Meow");
+    }
+}
